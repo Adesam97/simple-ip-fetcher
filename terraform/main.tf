7c9b405a68bafed05e25d4527144085946966e18
AdeSam97 <adejaresamson42@gmail.com>
2024-08-29 23:43:24 +0100

    final commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DC1ds_v3"
+    vm_size    = "Standard_DC2ds_v3"
   }
 
   identity {
