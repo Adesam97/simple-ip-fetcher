123d9a02ab47fdfdf447a44276e7396140464626
AdeSam97 <adejaresamson42@gmail.com>
2024-08-29 23:00:06 +0100

    final commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,7 +10,7 @@ provider "azurerm" {
 # Configure remote state
 terraform {
   backend "azurerm" {
-    resource_group_name  = azurerm_resource_group.aks_rg
+    resource_group_name  = "tfstate"
     storage_account_name = "tfstate${random_string.resource_code.result}"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
@@ -22,24 +22,4 @@ resource "random_string" "resource_code" {
   length  = 5
   special = false
   upper   = false
-}
-
-# Create storage account for tfstate
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.aks_rg
-  location                 = azurerm_resource_group.aks_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
-
-# Create container for tfstate
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
 }
