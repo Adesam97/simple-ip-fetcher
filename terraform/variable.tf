5a02a52398e2c418e552dd5933b77763fc965492
AdeSam97 <adejaresamson42@gmail.com>
2024-08-29 21:25:11 +0100

    final commit

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,11 @@
 variable "image" {
   description = "The ip-finder container image"
   type        = string
+  default     = "fikunmisamson/container-ip-finder:latest"
 }
 
 variable "region" {
   description = "region of deployment"
   type        = string
+  default     = "East US"
 }
